ef763223b7ee97bd141ef076f80807b756b37d11
Roman E <livcey@gmail.com>
2017-12-20 12:08:07 +0300

    Update runps.bat

--- runps.bat
+++ runps.bat
@@ -1,3 +1,3 @@
 cd "C:\Program Files (x86)\Digispot II\Uploader"
 echo %date% %time% %1 >> .\test.log
-powershell -NoProfile -ExecutionPolicy bypass -File "uploader-2.07b8.ps1" %1
+powershell -NoProfile -ExecutionPolicy bypass -File "uploader2.ps1" %1
