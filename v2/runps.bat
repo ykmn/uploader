053661787a8a9ea7d2a1ef337805fbcbd3ff4cbe
Roman E <livcey@gmail.com>
2020-02-25 18:52:12 +0300

    Readme

--- v2/runps.bat
+++ v2/runps.bat
@@ -1,7 +1,6 @@
 @echo off
-::cd "C:\Program Files (x86)\Digispot II\Uploader"
-cd C:\Users\r.ermakov\Documents\GitHub\uploader\
+cd "C:\Program Files (x86)\Digispot II\Uploader"
 title %1
 echo %date% %time% %1 >> log\runps.log
-::powershell -NoProfile -ExecutionPolicy bypass -File "uploader-2.07.015.ps1" %1 %2
+powershell -NoProfile -ExecutionPolicy bypass -File "uploader2.ps1" %1 %2
 ::pause
