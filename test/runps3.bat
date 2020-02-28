09085dab6b9119b45b8dfc914f77e40e8b773691
Roman E <livcey@gmail.com>
2020-02-28 11:38:59 +0300

    delete test folder

--- test/runps3.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-::cd "C:\Program Files (x86)\Digispot II\Uploader"
-cd C:\Users\r.ermakov\Documents\GitHub\uploader\
-title %1
-echo %date% %time% %1 >> log\runps.log
-powershell -NoProfile -ExecutionPolicy bypass -File "uploader3.00.001.ps1" %1 %2
-::pause
